b43efbeeda23497f8419abc9ff6e3dc144585b73
A.Beksultan <beksultan.amatov@alatoo.edu.kg>
2023-03-04 20:27:55 +0600

    My first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <div className='bg-indigo-600 w-full h-screen'></div>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
